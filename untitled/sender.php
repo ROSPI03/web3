63725e0ff623891af938bfc4ca36622d95ac24ae
ROSPI03 <105599241+ROSPI03@users.noreply.github.com>
2024-07-21 22:47:38 +0300

    Create sender.php

---
+++ untitled/sender.php
@@ -0,0 +1,24 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "sstamou@tuc.gr"; // Αντικαταστήστε με διεύθυνση email
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for contacting us!";
+    } else {
+        echo "There was an error sending your message. Please try again later.";
+    }
+} else {
+    echo "Invalid request";
+}
+
+?>
